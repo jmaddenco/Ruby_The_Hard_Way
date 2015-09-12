72f23f1d81481a762f47826a548fb7323c452983
jmaddenco <jmaddenco@gmail.com>
2015-09-12 16:42:44 -0600

    Create Exercise3.rb

---
+++ Exercise3.rb
@@ -0,0 +1,8 @@
+puts "I will cow count my chickens: "
+
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 -25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
