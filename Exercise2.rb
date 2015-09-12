21dd2c0f5c9e6140946104963d2f1dd404546215
jmaddenco <jmaddenco@gmail.com>
2015-09-12 16:42:18 -0600

    Create Exercise2.rb

---
+++ Exercise2.rb
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "dissable" or comment out a piece of code
+#pust "This won't run"
+
+puts "This will run."
